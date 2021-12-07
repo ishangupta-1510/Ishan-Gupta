dd5c8e84a51ff86ae5a4d8bf3f82ec379c9f1bd8
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2021-12-07 22:01:15 +0530

    first commit

---
+++ precache-manifest.fa853958a71b5b2d48df46e72ce31ae8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0d66f756e282723fc0e",
+    "url": "./static/css/main.46da8a0e.chunk.css"
+  },
+  {
+    "revision": "b0d66f756e282723fc0e",
+    "url": "./static/js/main.b0d66f75.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "./static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "62b0cb0f27b6df0953ccfdc299145cd5",
+    "url": "./index.html"
+  }
+];
